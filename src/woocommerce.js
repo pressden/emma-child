648d7f269dfb35f4267d886d0ccd5598f209d767
Eric Michel <eric.r.michel@gmail.com>
2022-01-27 11:25:20 -0500

    updated asset compiling to match changes in emma

---
+++ src/woocommerce.js
@@ -0,0 +1,9 @@
+/*--------------------------------------------------------------
+# Emma Child - Webpack WooCommerce Entrypoint
+--------------------------------------------------------------*/
+
+// CSS
+import './sass/woocommerce.scss';
+
+// JS
+import '../../emma/src/js/woocommerce';
